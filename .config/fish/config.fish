adadb0a31eff312dfda0ea2bc3b791aba8d53ef4
Aleksei Zaitsev <drshnitsel@gmail.com>
2024-05-02 13:15:17 +0400

    Add git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,13 @@ dotfiles config --local status.showUntrackedFiles no
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+starship init fish | source
+
+status --is-interactive; and rbenv init - fish | source
+
+set -x EDITOR nvim
+
+if [ -f $HOME/.config/fish/git.fish ]
+    source $HOME/.config/fish/git.fish
+end
