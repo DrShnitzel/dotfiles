0fa9aa4855b679ad789adfb9041bd33baf857f21
Aleksei Zaitsev <drshnitsel@gmail.com>
2024-10-10 19:07:36 +0400

    editors updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,17 @@
-# eval "$(/opt/homebrew/bin/brew shellenv)"
+set -U fish_greeting # Remove greeting
+
+# OSX only
+if test (uname) = Darwin
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    set -U fish_user_paths /opt/homebrew/opt/python@3.12/libexec/bin $fish_user_paths
+
+    set -U fish_user_paths /Users/drschnitzel/.local/bin $fish_user_paths
+    set -U fish_user_paths /Users/drschnitzel/go/bin $fish_user_paths
+    set -U fish_user_paths /Users/drschnitzel/.local/share/gem/ruby/3.3.0/bin $fish_user_paths
+end
 
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
 
 starship init fish | source
 
@@ -17,12 +23,6 @@ if [ -f $HOME/.config/fish/git.fish ]
     source $HOME/.config/fish/git.fish
 end
 
-# python binaries
-# set -U fish_user_paths /opt/homebrew/opt/python@3.12/libexec/bin $fish_user_paths
-
-# set -U fish_user_paths /Users/drschnitzel/.local/bin $fish_user_paths
-# set -U fish_user_paths /Users/drschnitzel/go/bin $fish_user_paths
-# set -U fish_user_paths /Users/drschnitzel/.local/share/gem/ruby/3.3.0/bin $fish_user_paths
-
 alias rubocop-fix="bundle exec --gemfile=gemfiles/rubocop.gemfile rubocop --config .rubocop.master.yml --fix (git diff --name-only HEAD~1)"
+
 set -x PATH $HOME/.rbenv/bin $PATH
